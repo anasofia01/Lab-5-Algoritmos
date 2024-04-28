65ac49529271e0999acf1e5ba7a2a1d7bc4d4c4b
anasofia01 <sofiapacheco0423@gmail.com>
2024-04-28 18:17:47 -0500

    corrijo mi función de traerme la info de la api

--- src/services/getProducts.ts
+++ src/services/getProducts.ts
@@ -1,7 +1,6 @@
 export async function getProducts() {
 	try {
-		const response = await fetch('https://fakestoreapi.com/products');
-		const products = await response.json();
+		const products = await fetch('https://fakestoreapi.com/products').then((res) => res.json());
 		return products;
 	} catch (error) {
 		console.error('error', error);
